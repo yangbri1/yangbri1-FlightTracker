80e6e490b2f5daad56df7c03ce5bb5f602d2db90
Brian Yang <yangbri1@msu.edu>
2025-01-02 22:45:25 +0000

    Working on 'updateFlight' function in FlightService.java class

--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -70,7 +70,15 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        return null;
+        // Boolean originalBool = null;
+        if(flightDAO.getFlightById(flight_id).flight_id == flight.flight_id){
+            flightDAO.updateFlight(flight_id, flight);
+            return flightDAO.getFlightById(flight_id);
+        }
+        else{
+            return null;
+        }
+        // return originalBoolean;
     }
 
     /**
